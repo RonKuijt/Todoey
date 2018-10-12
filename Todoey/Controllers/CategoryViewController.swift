b30d254588959c449751c664a0ab087bb7decc9c
Ron Kuijt <famkuyt@upcmail.nl>
2018-10-12 16:00:01 +0200

    After new category tableview

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,42 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Ron on 11/10/2018.
+//  Copyright © 2018 Ron. All rights reserved.
+//
+
+
+import UIKit
+import CoreData
+
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+   
+        
+        
+        }
+
+    //MARK: - TableView Datasource Methods
+    
+    
+    
+    //MARK: - TableView Delegate Methods
+    
+    
+    
+    //MARK: - Data Manipulation Methods
+    
+    
+    
+    
+    
+    
+}
